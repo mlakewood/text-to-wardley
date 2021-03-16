2edd0f52301ffaabac84f9ddd98a9c9af2935fa0
Mark Lakewood <underplank@gmail.com>
2021-03-16 15:56:27 -0700

    able to inject map nodes

--- src/cljs/text_to_wardley/views.cljs
+++ src/cljs/text_to_wardley/views.cljs
@@ -36,26 +36,27 @@
         x 300
         y 100
         ]
-    [:text {:x 280 :y 440 :fill "black"} "Print"]
-    [:circle {:cx x :cy y :r "5" :stroke "black" :stroke-width "3" :fill "white"}]
+    [
+     [:text {:x (- x 10) :y (- y 20) :fill "black"} "Print"]
+     [:circle {:cx x :cy y :r "5" :stroke "black" :stroke-width "3" :fill "white"}]]
     ))
 
-(defn diagram [map-nodes]
-  [:svg {:style {:border "1px solid" :background "white" :width "50%" :height "90%"}}
-   [:line {:x1 "55" :y1 "30" :x2 "55" :y2 "420" :style {:stroke-width 2 :stroke "black"}}]
-   [:line {:x1 "240" :y1 "30" :x2 "240" :y2 "420" :stroke-dasharray "5,5" :style {:stroke-width 2 :stroke "grey"}}]
-   [:line {:x1 "425" :y1 "30" :x2 "425" :y2 "420" :stroke-dasharray "5,5" :style {:stroke-width 2 :stroke "grey"}}]
-   [:line {:x1 "610" :y1 "30" :x2 "610" :y2 "420" :stroke-dasharray "5,5" :style {:stroke-width 2 :stroke "grey"}}]
-   [:line {:x1 "55" :y1 "420" :x2 "800" :y2 "420" :style {:stroke-width 2 :stroke "black"}}]
-   [:text {:x 5 :y 45 :fill "black" :transform "rotate(270) translate(-90,5)"} "Visible"]
-   [:text {:x 5 :y 200 :fill "black" :font-weight "bold" :transform "rotate(270) translate(-270,-150)"} "Value Chain"]
-   [:text {:x 5 :y 400 :fill "black" :transform "rotate(270) translate(-420,-350)"} "Invisible"]
-   [:text {:x 65 :y 440 :fill "black"} "Genesis"]
-   [:text {:x 250 :y 440 :fill "black"} "Custom Built"]
-   [:text {:x 440 :y 440 :fill "black"} "Product (+ rental)"]
-   [:text {:x 620 :y 440 :fill "black"} "Commodity ( + utility)"]
-   map-nodes
-   ])
+(defn diagram []
+  (let [background [:svg {:style {:border "1px solid" :background "white" :width "50%" :height "90%"}}
+                    [:line {:x1 "55" :y1 "30" :x2 "55" :y2 "420" :style {:stroke-width 2 :stroke "black"}}]
+                    [:line {:x1 "240" :y1 "30" :x2 "240" :y2 "420" :stroke-dasharray "5,5" :style {:stroke-width 2 :stroke "grey"}}]
+                    [:line {:x1 "425" :y1 "30" :x2 "425" :y2 "420" :stroke-dasharray "5,5" :style {:stroke-width 2 :stroke "grey"}}]
+                    [:line {:x1 "610" :y1 "30" :x2 "610" :y2 "420" :stroke-dasharray "5,5" :style {:stroke-width 2 :stroke "grey"}}]
+                    [:line {:x1 "55" :y1 "420" :x2 "800" :y2 "420" :style {:stroke-width 2 :stroke "black"}}]
+                    [:text {:x 5 :y 45 :fill "black" :transform "rotate(270) translate(-90,5)"} "Visible"]
+                    [:text {:x 5 :y 200 :fill "black" :font-weight "bold" :transform "rotate(270) translate(-270,-150)"} "Value Chain"]
+                    [:text {:x 5 :y 400 :fill "black" :transform "rotate(270) translate(-420,-350)"} "Invisible"]
+                    [:text {:x 65 :y 440 :fill "black"} "Genesis"]
+                    [:text {:x 250 :y 440 :fill "black"} "Custom Built"]
+                    [:text {:x 440 :y 440 :fill "black"} "Product (+ rental)"]
+                    [:text {:x 620 :y 440 :fill "black"} "Commodity ( + utility)"]]
+        nodes (map-nodes)]
+    (into [] (concat background nodes))))
 
 (defn link-to-about-page []
   [re-com/hyperlink
